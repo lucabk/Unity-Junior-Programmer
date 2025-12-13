67c47baf2413d3f45750c550505aeb3c0dd5f563
Luca L <132922066+lucabk@users.noreply.github.com>
2025-12-13 12:50:11 +0100

    Prototype 1 - Ex

---
+++ Prototype 1 - Ex/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 10.0f;
+    public float rotationSpeed = 18.0f;
+    private float verticalInput;
+    private Transform propeller;
+
+    private float propellerSpeed = 400.0f;
+
+    void Start()
+    {
+        propeller = transform.Find("Propeller");
+        if (propeller == null) Debug.LogError("Propeller NOT FOUND");
+    }
+
+    void FixedUpdate()
+    {
+        // get the user input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed * verticalInput);
+
+        if (propeller) propeller.Rotate(Vector3.forward * Time.deltaTime * propellerSpeed);
+    }
+}
