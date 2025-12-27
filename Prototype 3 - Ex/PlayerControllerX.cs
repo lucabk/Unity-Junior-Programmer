82591497d6039fde6e1cffc18d3e96c8833db048
Luca L <132922066+lucabk@users.noreply.github.com>
2025-12-27 11:48:57 +0100

    Prototype 3 - Ex

---
+++ Prototype 3 - Ex/PlayerControllerX.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce = 0.8f;
+    private float gravityModifier = 1.5f;
+    private float marginTop = 15;
+    private bool isLowEnough = false;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip bounceSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfBelowMaxHeight();
+
+        // While space is pressed and player is low enough, float up
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
+        {
+            playerRb.AddForce(Vector3.up * floatForce, ForceMode.Impulse);
+        }
+        else if (!isLowEnough)
+        {
+            transform.position = new Vector3(transform.position.x, marginTop, transform.position.z);
+            // stop upward momentum when hitting top
+            if (playerRb.linearVelocity.y > 0f)
+                playerRb.linearVelocity = new Vector3(playerRb.linearVelocity.x, 0f, playerRb.linearVelocity.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        } 
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            Destroy(other.gameObject);
+        }
+
+        // if player collides with ground, bounce off
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+            playerRb.AddForce(Vector3.up * floatForce * 2, ForceMode.Impulse);
+        }
+    }
+
+    private void CheckIfBelowMaxHeight()
+    {
+        isLowEnough = transform.position.y < marginTop;
+    }
+}
